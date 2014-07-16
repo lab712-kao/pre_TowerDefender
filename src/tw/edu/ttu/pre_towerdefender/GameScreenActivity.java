c975fd100e19d41a77b407b91a278357ca2ec518
almostsank <almostsank@gmail.com>
2014-07-16 17:21:18 +0800

    first commit

---
+++ src/tw/edu/ttu/pre_towerdefender/GameScreenActivity.java
@@ -0,0 +1,95 @@
+package tw.edu.ttu.pre_towerdefender;
+
+import java.io.IOException;
+import com.metaio.sdk.ARViewActivity;
+import com.metaio.sdk.MetaioDebug;
+import com.metaio.sdk.jni.IGeometry;
+import com.metaio.sdk.jni.IMetaioSDKCallback;
+import com.metaio.sdk.jni.Rotation;
+import com.metaio.sdk.jni.Vector3d;
+import com.metaio.tools.io.AssetsManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.Button;
+
+public class GameScreenActivity extends ARViewActivity {
+
+	private IGeometry tower_1, tower_2;
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.game_screen, menu);
+		return true;
+	}
+
+	@Override
+	protected int getGUILayout() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected IMetaioSDKCallback getMetaioSDKCallbackHandler() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void loadContents() {
+		try {
+			AssetsManager.extractAllAssets(this, true);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		setRequestedOrientation(0);
+		
+		try {
+			String trackingConfigFile = AssetsManager.getAssetPath("TrackingData_MarkerlessFast.xml");
+			boolean result = metaioSDK.setTrackingConfiguration(trackingConfigFile); 
+			MetaioDebug.log("Tracking data loaded: " + result);
+			
+			Log.d("trackingData", "Tracking data loaded: " + result);
+			
+			String towerModel1 = AssetsManager.getAssetPath("saintriqT3DS.obj");
+			String towerModel2 = AssetsManager.getAssetPath("FIRSTtower.obj");
+			
+			if ((towerModel1 != null) && (towerModel2 != null)) {
+				
+				tower_1 = metaioSDK.createGeometry(towerModel1);
+				tower_2 = metaioSDK.createGeometry(towerModel2);
+				
+				if(tower_1 != null) {
+					tower_1.setScale(new Vector3d(35.0f, 35.0f, 35.0f));
+					tower_1.setRotation(new Rotation((float)(Math.PI/2), 0.0f, 0.0f));
+					tower_1.setCoordinateSystemID(1);
+				}
+				else {
+					MetaioDebug.log(Log.ERROR, "Error loading geometry: " + towerModel1);
+				}
+				
+				if(tower_2 != null) {
+					tower_2.setScale(new Vector3d(35.0f, 35.0f, 35.0f));
+					tower_1.setRotation(new Rotation((float)(Math.PI/2), 0.0f, 0.0f));
+					tower_1.setCoordinateSystemID(2);
+				}
+				else {
+					MetaioDebug.log(Log.ERROR, "Error loading geometry: " + towerModel2);
+				}
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void onGeometryTouched(IGeometry geometry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/tw/edu/ttu/pre_towerdefender/MainActivity.java
@@ -0,0 +1,48 @@
+package tw.edu.ttu.pre_towerdefender;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	
+	Button startBtn;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		startBtn = (Button)findViewById(R.id.startBtn);
+		
+		if(startBtn == null) {
+			System.out.println("where is the button?");
+		}
+		else {
+			startBtn.setOnClickListener(new Button.OnClickListener(){
+
+				@Override
+				public void onClick(View v) {
+					gotoGameActivity(v);
+				}         
+
+	        });
+		}
+	}
+	
+	public void gotoGameActivity(View v) {
+		Intent it = new Intent();
+		it.setClass(this, GameScreenActivity.class);
+		startActivity(it);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
