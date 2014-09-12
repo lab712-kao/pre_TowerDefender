b505bc91472f604dc44ed5ca4db1fd15961713f8
Os-carsun <good0991@gmail.com>
2014-09-12 15:57:16 +0800

    compelet tower class

--- src/gameObject/tower/tower.java
+++ src/gameObject/tower/tower.java
@@ -13,11 +13,16 @@ import com.metaio.tools.io.AssetsManager;
 public class tower extends object{
 	
 	private float atk;
-	public tower(IGeometry model, float x, float y, float health) {
+	public tower(IGeometry model, float x, float y, float health, float atk) {
 		super(model, x, y, health);
+		this.atk = atk; 
 		// TODO Auto-generated constructor stub
 	}
-	
+	public tower(IGeometry model, float x, float y, float health) {
+		super(model, x, y, health);
+		this.atk = 0;
+		// TODO Auto-generated constructor stub
+	}
 	public void attack(object other){
 		if(other.getHealth()-atk<=0){
 			other.dead();
