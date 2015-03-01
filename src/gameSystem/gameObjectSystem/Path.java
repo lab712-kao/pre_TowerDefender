3aa130dd9e4fd48d185e3b20d70b206e01431d59
Os-carsun <good0991@gmail.com>
2015-03-01 19:01:27 +0800

    complete path class

--- src/gameSystem/gameObjectSystem/Path.java
+++ src/gameSystem/gameObjectSystem/Path.java
@@ -17,6 +17,7 @@ public class Path {
 	
 	private class PathPoint implements Comparator<PathPoint>{
 		private Vector3d position,begin,end;
+		private PathPoint nextPoint = null;
 		private float angle;
 		private boolean ignore = false;
 		private boolean xAscending = true;
@@ -39,6 +40,9 @@ public class Path {
 		public float getAngle(){
 			return angle;
 		}
+		public void setAngle(float angle){
+			this.angle = angle;
+		}
 		public Vector3d getPosition(){
 			return position;
 		}
@@ -47,6 +51,12 @@ public class Path {
 		}
 		public void setIgnore(boolean ignore){
 			this.ignore = ignore;
+		}
+		public void setNextPoint(PathPoint nextPoint){
+			this.nextPoint = nextPoint;
+		}
+		public PathPoint getNextPoint(){
+			return nextPoint;
 		}
 		@Override
 		public int compare(PathPoint lhs, PathPoint rhs) {
@@ -101,12 +111,46 @@ public class Path {
 	
 	public void addPathPoint(Vector3d pos){
 		way.add(new PathPoint(pos, DEFAULT_ANGLE));
+		calPath();
+	}
+	private float calAngle(Vector3d A){
+		
+		if(A!=null)
+			return (float) Math.atan2(A.getY(), A.getX());
+		return DEFAULT_ANGLE;
 		
 	}
 	private void calPath(){
 		PathPoint point[] = new PathPoint[way.size()];
 		Arrays.sort(point, compator);
+		way.clear();
+		for (int i = 0; i < point.length-1; i++) {
+			point[i].setNextPoint(point[i+1]);
+			point[i].setAngle(calAngle(point[i+1].getPosition()));
+			way.add(point[i]);
+		}
+		point[point.length-1].setAngle(calAngle(end));
+	}
+	public PathPoint getNextPathPoint(PathPoint nowPoint){
+		
+		if(nowPoint == null){//it mean in the begin
+			return way.get(0);
+		}
+		
+		int i = way.indexOf(nowPoint);
+		if(i==-1 || i == way.size())// no this point
+		 return null;
+		else if(way.get((i+1)%way.size()).isIgnore() == true)//if ignore get nextPoit
+			return getNextPathPoint(way.get((i+1)%way.size()));
+		
+		return way.get((i+1)%way.size());
 		
 	}
-	
+	public void removePathPoint(PathPoint point){
+		int i = way.indexOf(point);
+		if(i!=-1){//fake delete
+			way.get(i).setIgnore(true);
+		}
+		calPath();
+	}
 }
